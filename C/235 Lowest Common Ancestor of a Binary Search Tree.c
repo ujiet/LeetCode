de3d4715212dc0566f9f3a0bd0f3fab9355e3d59
ujiet <36890009+ujiet@users.noreply.github.com>
2018-07-29 02:28:54 +0800

    Update 235 Lowest Common Ancestor of a Binary Search Tree.c

--- C/235 Lowest Common Ancestor of a Binary Search Tree.c
+++ C/235 Lowest Common Ancestor of a Binary Search Tree.c
@@ -5,6 +5,11 @@
  *     struct TreeNode *left;
  *     struct TreeNode *right;
  * };
+ * 
+ * To find ount the LCA in a BST, we can imagine there are 2 roads in the BST, roadP and roadQ.
+ * Each one is the traversing path when searching for the value of p or q. We know that the search path for   
+ * each node is unique, and since the LCA is the ancestor of both nodes, so LCA is exist in the both roads.   
+ * So what we need to do is find the split point of the path, and it will be the anwser.
  */
  
 struct TreeNode* lowestCommonAncestor(struct TreeNode* root, struct TreeNode* p, struct TreeNode* q) {
